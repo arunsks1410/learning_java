3eeba2a6bfad05efacda3cfee223d7e05fd8e6ad
arunsks1410 <arunsks1410@gmail.com>
2025-08-09 13:03:39 +0530

    learned dataTypes

--- Hello.java
+++ Hello.java
@@ -4,9 +4,11 @@ public class Hello {
     public static void main(String[] args) {
         // variables
         int num1 = 10;
-    
-        int num2 = 12;
-        int result = num1 + num2;
-        System.out.println(result);
+        float f = 20.18f;
+        double d = 13.40;
+        long l = 4033343l;
+        char a = 'r';
+        String name = "Arunkumar";
+        boolean bool = true;
     }
 }
