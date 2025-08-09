6bcceb9cbef402986595326b1ec582102671cc26
arunsks1410 <arunsks1410@gmail.com>
2025-08-09 14:14:12 +0530

    learned casting and conversion

--- Hello.java
+++ Hello.java
@@ -1,14 +1,19 @@
-// 1:04:52
-
+// 1:17:49
 public class Hello {
     public static void main(String[] args) {
-        // variables
-        int num1 = 10;
-        float f = 20.18f;
-        double d = 13.40;
-        long l = 4033343l;
-        char a = 'r';
-        String name = "Arunkumar";
-        boolean bool = true;
+        byte a = 127;
+        System.out.println(a);
+
+        int b = 230;
+        System.out.println(b);
+
+        b = a; // inplicit conversion also called (conversion)
+        System.out.println(b);
+
+        int m = 300;
+
+        byte n = (byte) m; // Explicit conversion (casting)
+
+        System.out.println(n); // 44 => 300 % 256(byte total value)
     }
 }
