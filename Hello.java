d79b89ea758db963a0757bc86fdf3a2c186f2327
arunsks1410 <arunsks1410@gmail.com>
2025-08-09 12:59:49 +0530

    learning java varibales and types

---
+++ Hello.java
@@ -0,0 +1,12 @@
+// 1:04:52
+
+public class Hello {
+    public static void main(String[] args) {
+        // variables
+        int num1 = 10;
+    
+        int num2 = 12;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
