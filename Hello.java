e4a5fc417041eed18dc1388bb14ade945bc1982c
arunsks1410 <arunsks1410@gmail.com>
2025-08-09 14:16:32 +0530

    added range values

--- Hello.java
+++ Hello.java
@@ -1,13 +1,25 @@
 // 1:17:49
 public class Hello {
     public static void main(String[] args) {
+
+        /*
+         * byte Stores whole numbers from -128 to 127
+         * short Stores whole numbers from -32,768 to 32,767
+         * int Stores whole numbers from -2,147,483,648 to 2,147,483,647
+         * long Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+         * float Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+         * double Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits
+         * boolean Stores true or false values
+         * char Stores a single character/letter or ASCII values
+         */
+
         byte a = 127;
         System.out.println(a);
 
         int b = 230;
         System.out.println(b);
 
-        b = a; // inplicit conversion also called (conversion)
+        b = a; // implicit conversion also called (conversion)
         System.out.println(b);
 
         int m = 300;
