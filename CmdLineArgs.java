a50d6cfdd0a866d3d29021433871f775084aca57
arunsks1410 <arunsks1410@gmail.com>
2025-08-19 17:52:05 +0530

    learned java identifires

--- CmdLineArgs.java
+++ CmdLineArgs.java
@@ -1,9 +1,18 @@
 public class CmdLineArgs {
     public static void main(String[] args) {
+
+        System.out.println(args.length);
+        System.out.println(args[0]);
+
+        if (args.length == 0) {
+            System.out.println("warning! No Arguments passed");
+            return;
+        }
+
         String name = args[0];
         String location = args[1];
 
-        System.out.println("welcome to " + name + " academy and it's located in " + location);
-        // System.out.println("args 1 : " + args[0]);
+        System.out.println("welcome to " + name + " academy and it's located in " +
+                location);
     }
 }
---
+++ EmployeeDetails.java
@@ -0,0 +1,12 @@
+public class EmployeeDetails {
+    public static void main(String[] args) {
+        String id = args[0];
+        String name = args[1];
+        String phNo = args[2];
+        String email = args[3];
+        String citizen = args[4];
+        String amountInBankAccount = args[5];
+        System.out.println("Employee id : " + id + " name : " + name + " mobile number : " + phNo + " email : " + email
+                + " AmountInUSD : " + amountInBankAccount + " citizenship : " + citizen);
+    }
+}
