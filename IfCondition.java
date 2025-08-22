d4baf2359b8d0b4f924ba6ccc9d02623f34aede7
arunsks1410 <arunsks1410@gmail.com>
2025-08-22 14:45:44 +0530

    learned if-conditions

--- IfCondition.java
+++ IfCondition.java
@@ -1,14 +1,36 @@
-// Class Work
+import java.util.Scanner;
 
 class IfCondition {
 
+    private static double calculateShippingCost(double orderedAmount) {
+        System.out.println("Ordered Amount : " + orderedAmount);
+        if (orderedAmount > 5000) {
+            return 0;
+        } else if (orderedAmount >= 2000 && orderedAmount <= 5000) {
+            return 100;
+        } else {
+            return 200;
+        }
+
+    }
+
     public static void main(String[] args) {
+        // Class Work
         int num = 26;
         if (num % 2 == 0) {
             System.out.println("Even number");
         } else {
             System.out.println("odd number");
         }
+
+        // Home Work
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter orderAmount");
+        double orderAmount = sc.nextDouble();
+
+        double shippingCost = IfCondition.calculateShippingCost(orderAmount);
+        System.out.println("shipping cost : " + shippingCost);
+
     }
 
 }
