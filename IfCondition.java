3c1ee2828b28262f3db2a1eff91db85267473b98
arunsks1410 <arunsks1410@gmail.com>
2025-08-22 14:31:46 +0530

    if statement learned

---
+++ IfCondition.java
@@ -0,0 +1,14 @@
+// Class Work
+
+class IfCondition {
+
+    public static void main(String[] args) {
+        int num = 26;
+        if (num % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("odd number");
+        }
+    }
+
+}
