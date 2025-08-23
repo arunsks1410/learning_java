4cbcdb9d0a0fb9e2d413c73268568562504a3a91
arunsks1410 <arunsks1410@gmail.com>
2025-08-23 10:55:24 +0530

    learned strings

--- StringExample.java
+++ StringExample.java
@@ -19,5 +19,50 @@ class StringExample {
         System.out.println(str3 == str4);
         System.out.println(str5 == "Javascript");
 
+        // Interview experience
+        String name = "Arunkumar";
+        System.out.println("original string : " + name);
+        System.out.println("first occurence of r : " + name.indexOf("r"));
+        System.out.println("first occurence of kumar : " + name.indexOf("kumar"));
+
+        System.out.println("last occurence of u : " + name.lastIndexOf("u"));
+
+        System.out.println("replace m -> w : " + name.replace("m", "w"));
+
+        String languages = "Tamil,Hindi,Telugu,Urudu,Malayalam";
+        String[] lang = languages.split(",");
+        System.out.println("splitted string : ");
+        for (String language : lang) {
+            System.out.println("language : " + language);
+        }
+        // System.out.println("lang : " + lang);
+
+        // System.out.println("split in console : " + languages.split(","));
+
+        String location = "    bangalore    ";
+        System.out.println("location : " + location);
+        System.out.println("location trim : " + location.trim());
+
+        // Class Work
+        String state = "Tamilnadu";
+        String city = "Salem";
+        System.out.println("state  : " + state + "city : " + city);
+        System.out.println("Char At 3 : " + state.charAt(3));
+        System.out.println("Sub String : 5 - 9 : " + state.substring(5, 9)); // nadu
+        System.out.println("concat : " + state.concat(city));
+
+        // HomeWork
+        String color = "sky blue";
+        char[] result = color.toCharArray();
+        for (char ch : result) {
+            System.out.println("ch : " + ch);
+        }
+        // System.out.println(result);
+
+        System.out.println(state.contains("Tamil"));
+
+        String fileName = "IMG_12345hplap.jpg";
+        System.out.println(fileName.startsWith("IMG_") && fileName.endsWith(".jpg"));
+
     }
 }
